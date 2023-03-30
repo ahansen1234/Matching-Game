bd91c0b18e06eb46b1e4a20d69fb4c97f282e4d0
Alexander Martinez Hansen <alex.c.hansen@gmail.com>
2023-03-30 14:04:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import shuffle from './utilities/shuffle';
+import Card from './components/Card';
+import { useState } from 'react';
 
 function App() {
+  const [cards, setCards] = useState(shuffle);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      {cards.map((card) => {
+          const { image, id, matched } = card;
+          
+          return (
+            <Card
+              key={id}
+              image={image}
+              selected={false}
+              onClick={() => {}}
+            />
+          );
+        })}
     </div>
   );
 }
---
+++ src/utilities/shuffle.js
@@ -0,0 +1,17 @@
+const shuffle = () => {
+    const assets = [
+        { image: '/assets/beach.png' },
+        { image: '/assets/city.png' },
+        { image: '/assets/grassy-hills.png' },
+        { image: '/assets/rainforest.png' },
+        { image: '/assets/Sand-dunes.png' },
+        { image: '/assets/siberia-forest.png' },
+        { image: '/assets/Snowy-Mountains.png' },
+        { image: '/assets/Temperate-Forest.png' },
+    ];
+    return [...assets, ...assets]
+        .sort(() => Math.random - .5)
+        .map((card) => ({ ...card, id: Math.random() }));
+};
+
+export default shuffle;
