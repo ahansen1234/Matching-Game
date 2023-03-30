b8d986173ce84d7b9feadf016b21d8dcb4c15a31
Alexander Martinez Hansen <alex.c.hansen@gmail.com>
2023-03-30 15:21:20 -0700

    fixed randomization

--- src/utilities/shuffle.js
+++ src/utilities/shuffle.js
@@ -9,7 +9,7 @@ const shuffle = () => {
         { image: '/assets/Snowy-Mountains.png' },
         { image: '/assets/Temperate-Forest.png' },
     ];
-    return [...assets, ...assets].sort(() => Math.random - 0.5).map((card) => ({ ...card, id: Math.random() }));
+    return [...assets, ...assets].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }));
 };
 
 export default shuffle;
