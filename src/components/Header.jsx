27677770d11deb36f392b95f6e89510815cdd162
Alexander Martinez Hansen <alex.c.hansen@gmail.com>
2023-03-30 15:13:04 -0700

    final push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 const Header = ({ handleNewGame, wins }) => {
-    useEffect(() => (document.title = `${wins} wins`), [wins]);
+    useEffect(() => { document.title = `${wins} wins` }, [wins]);
 
     return (
         <header className="header">
